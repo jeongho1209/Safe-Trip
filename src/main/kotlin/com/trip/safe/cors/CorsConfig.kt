5fc5502d86dbb323c62fe300614fa66cea21f9e4
이정호 <dlwjdgh1115@gmail.com>
2024-03-13 18:25:23 +0900

    add : corsconfig

---
+++ src/main/kotlin/com/trip/safe/cors/CorsConfig.kt
@@ -0,0 +1,17 @@
+package com.trip.safe.cors
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.reactive.config.CorsRegistry
+import org.springframework.web.reactive.config.WebFluxConfigurer
+
+@Configuration
+class CorsConfig : WebFluxConfigurer {
+
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry
+            .addMapping("/**")
+            .allowedOrigins("*")
+            .allowedMethods("*")
+            .allowedHeaders("*")
+    }
+}

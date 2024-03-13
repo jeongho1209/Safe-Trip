367329c1218cd0dde9df2bec27163eafe4f7def1
이정호 <dlwjdgh1115@gmail.com>
2024-03-13 18:39:33 +0900

    add : CorsConfig

--- src/main/kotlin/com/trip/safe/common/cors/CorsConfig.kt
+++ src/main/kotlin/com/trip/safe/common/cors/CorsConfig.kt
@@ -1,4 +1,4 @@
-package com.trip.safe.cors
+package com.trip.safe.common.cors
 
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.reactive.config.CorsRegistry

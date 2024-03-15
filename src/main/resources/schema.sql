2fa0f375de77e24bb4aea81890b37a0649002101
이정호 <dlwjdgh1115@gmail.com>
2024-03-15 09:48:23 +0900

    add : user table schema

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,1 +1,8 @@
-# TODO Table Schema
+CREATE TABLE IF NOT EXISTS user
+(
+    id         BIGINT      NOT NULL,
+    account_id VARCHAR(20) UNIQUE NOT NULL,
+    password   CHAR(60)    NOT NULL,
+    age        TINYINT UNSIGNED NOT NULL,
+    PRIMARY KEY (id)
+);

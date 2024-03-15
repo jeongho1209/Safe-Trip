8a76eb1da48d6c75e3e6f4c581df2bc24616317f
이정호 <dlwjdgh1115@gmail.com>
2024-03-15 09:48:57 +0900

    add : configuration

--- src/main/kotlin/com/trip/safe/SafeTripServerApplication.kt
+++ src/main/kotlin/com/trip/safe/SafeTripServerApplication.kt
@@ -1,8 +1,10 @@
 package com.trip.safe
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 
+@ConfigurationPropertiesScan
 @SpringBootApplication
 class SafeTripServerApplication
 

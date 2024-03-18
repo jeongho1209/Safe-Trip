7e870c28a28a64af7dee4cb0c40e85826fd66298
이정호 <dlwjdgh1115@gmail.com>
2024-03-18 10:15:02 +0900

    chore : r2dbcrepository 제거

--- src/main/kotlin/com/trip/safe/common/r2dbc/R2dbcConfig.kt
+++ src/main/kotlin/com/trip/safe/common/r2dbc/R2dbcConfig.kt
@@ -5,11 +5,9 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.core.io.ClassPathResource
 import org.springframework.data.r2dbc.config.AbstractR2dbcConfiguration
-import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories
 import org.springframework.r2dbc.connection.init.ConnectionFactoryInitializer
 import org.springframework.r2dbc.connection.init.ResourceDatabasePopulator
 
-@EnableR2dbcRepositories
 @Configuration
 class R2dbcConfig(
     private val connectionFactory: ConnectionFactory,

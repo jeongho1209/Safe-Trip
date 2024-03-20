3f079c295d298ac4fa70c5c998481ab66af16291
이정호 <dlwjdgh1115@gmail.com>
2024-03-20 14:57:56 +0900

    chore: router-functions -> controller 변경

---
+++ src/main/kotlin/com/trip/safe/review/presentation/ReviewController.kt
@@ -0,0 +1,28 @@
+package com.trip.safe.review.presentation
+
+import com.trip.safe.review.presentation.dto.request.CreateReviewRequest
+import com.trip.safe.review.service.ReviewService
+import jakarta.validation.Valid
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.bind.annotation.RestController
+
+@RequestMapping("/reviews")
+@RestController
+class ReviewController(
+    private val reviewService: ReviewService,
+) {
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("/{travel-destination-id}")
+    suspend fun createReview(
+        @RequestBody @Valid request: CreateReviewRequest,
+        @PathVariable("travel-destination-id") travelDestinationId: Long,
+    ) {
+        reviewService.createReview(request, travelDestinationId)
+    }
+}
---
+++ src/main/kotlin/com/trip/safe/user/presentation/UserController.kt
@@ -0,0 +1,35 @@
+package com.trip.safe.user.presentation
+
+import com.trip.safe.user.presentation.dto.request.UserSignInRequest
+import com.trip.safe.user.presentation.dto.request.UserSignUpRequest
+import com.trip.safe.user.presentation.dto.response.TokenResponse
+import com.trip.safe.user.service.UserService
+import jakarta.validation.Valid
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.bind.annotation.RestController
+
+@RequestMapping("/users")
+@RestController
+class UserController(
+    private val userService: UserService,
+) {
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("/signup")
+    suspend fun signUp(
+        @RequestBody @Valid request: UserSignUpRequest
+    ): TokenResponse {
+        return userService.signUp(request)
+    }
+
+    @PostMapping("/login")
+    suspend fun signIn(
+        @RequestBody @Valid request: UserSignInRequest
+    ): TokenResponse {
+        return userService.signIn(request)
+    }
+}

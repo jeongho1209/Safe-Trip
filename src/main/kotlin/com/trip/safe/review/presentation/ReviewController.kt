ceb3c94e8740c14d7e8978566fcc0cc3cba677de
이정호 <dlwjdgh1115@gmail.com>
2024-03-20 23:26:42 +0900

    feat: Review List 조회 기능

--- src/main/kotlin/com/trip/safe/review/presentation/ReviewController.kt
+++ src/main/kotlin/com/trip/safe/review/presentation/ReviewController.kt
@@ -1,9 +1,11 @@
 package com.trip.safe.review.presentation
 
 import com.trip.safe.review.presentation.dto.request.CreateReviewRequest
+import com.trip.safe.review.presentation.dto.response.ReviewListResponse
 import com.trip.safe.review.service.ReviewService
 import jakarta.validation.Valid
 import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -24,5 +26,12 @@ class ReviewController(
         @PathVariable("travel-destination-id") travelDestinationId: Long,
     ) {
         reviewService.createReview(request, travelDestinationId)
+    }
+
+    @GetMapping("/{travel-destination-id}")
+    suspend fun getReviewsByTravelDestinationId(
+        @PathVariable("travel-destination-id") travelDestinationId: Long,
+    ): ReviewListResponse {
+        return reviewService.getReviewsByTravelDestinationId(travelDestinationId)
     }
 }

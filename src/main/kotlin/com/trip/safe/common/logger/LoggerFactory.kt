9040edfa6ece21e6b47ba09181be5ebdc4f0011f
이정호 <dlwjdgh1115@gmail.com>
2024-03-22 21:00:27 +0900

    add: LoggerFactory

--- src/main/kotlin/com/trip/safe/common/logger/LoggerFactory.kt
+++ src/main/kotlin/com/trip/safe/common/logger/LoggerFactory.kt
@@ -1,5 +1,6 @@
 package com.trip.safe.common.logger
 
 import org.slf4j.LoggerFactory
+import java.util.logging.Logger
 
-inline fun <reified T> T.logger() = LoggerFactory.getLogger(T::class.java)!!
+inline fun <reified T> T.logger(): Logger = LoggerFactory.getLogger(T::class.java)!!

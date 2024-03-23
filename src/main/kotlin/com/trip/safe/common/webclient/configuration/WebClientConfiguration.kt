05489cec684876a44098f991c4915a5214bba80b
이정호 <dlwjdgh1115@gmail.com>
2024-03-23 09:47:52 +0900

    add: Webclient COnfig

---
+++ src/main/kotlin/com/trip/safe/common/webclient/configuration/WebClientConfiguration.kt
@@ -0,0 +1,31 @@
+package com.trip.safe.common.webclient.configuration
+
+import com.trip.safe.common.logger.logger
+import org.slf4j.Logger
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.reactive.function.client.WebClient
+import org.springframework.web.util.DefaultUriBuilderFactory
+
+@Configuration
+class WebClientConfiguration(
+    @Value("\${service.url}")
+    private val serviceUrl: String,
+) {
+    private val log: Logger = logger()
+
+    @Bean
+    fun defaultUriFactory() = DefaultUriBuilderFactory(serviceUrl).apply {
+        encodingMode = DefaultUriBuilderFactory.EncodingMode.VALUES_ONLY
+    }
+
+    @Bean
+    fun webClient() = WebClient.builder()
+        .uriBuilderFactory(defaultUriFactory())
+        .filter { request, next ->
+            log.info("try to request webclient url : ${request.url()}, method : ${request.method()}")
+            next.exchange(request)
+        }
+        .build()
+}

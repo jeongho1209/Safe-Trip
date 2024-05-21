029aad7cec3963bab0c186f6e90ea5ab8b4475df
이정호 <dlwjdgh1115@gmail.com>
2024-05-21 20:08:52 +0900

    fix: constant header 수정

--- src/main/kotlin/com/trip/safe/common/security/jwt/JwtConstants.kt
+++ src/main/kotlin/com/trip/safe/common/security/jwt/JwtConstants.kt
@@ -1,7 +1,7 @@
 package com.trip.safe.common.security.jwt
 
 object JwtConstants {
-    const val HEADER = "Authentication"
+    const val HEADER = "Authorization"
     const val PREFIX = "Bearer "
     const val TOKEN_TYPE = "accessToken"
 }
